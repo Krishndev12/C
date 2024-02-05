a33312c30ec854b41639b832feb1c20ddaaceb13
Krishndev12 <159033800+Krishndev12@users.noreply.github.com>
2024-02-05 21:47:18 +0530

    Update krishna.cpp

--- krishna.cpp
+++ krishna.cpp
@@ -2,7 +2,7 @@ import util.java .seanner ;
 class name krishna .java;
 {
     // Add comments
-    var a = 10;
+    var a = 4890;
 }
 public static void main ("krishna.java");
 system.in .println ("krishna .java");
