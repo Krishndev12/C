fbad090ad04c6705760d20417a6d44f83fb1803d
krishndev <sawkrishna983@gmail.com>
2024-02-05 21:46:24 +0530

    Add var

--- krishna.cpp
+++ krishna.cpp
@@ -1,7 +1,8 @@
 import util.java .seanner ;
 class name krishna .java;
 {
-    
+    // Add comments
+    var a = 10;
 }
 public static void main ("krishna.java");
 system.in .println ("krishna .java");
